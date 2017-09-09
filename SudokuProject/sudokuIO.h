35a9b9dc10de5cd90cd238ea96c501e7ab46e19c
Wasdns <wasdnsxchen@gmail.com>
2017-09-09 12:00:05 +0800

    update sudokuIO.h: fix sudoku IO bottleneck

--- SudokuProject/sudokuIO.h
+++ SudokuProject/sudokuIO.h
@@ -1,13 +1,14 @@
 #ifndef IOER_H
 #define IOER_H
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 class SudokuIOer {
 public:
     SudokuIOer() {
     }
-    void outputFile(int solution[9][9], char filename[]);
+    void outputFile(int solution[9][9], fstream sudokuFile);
 };
 
 #endif
