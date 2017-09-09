1f30c7e422057af93a5c3e78ea7c139901335d38
Wasdns <wasdnsxchen@gmail.com>
2017-09-09 13:09:12 +0800

    update sudokuIO.h: using ofstream& instead of ofstream

--- SudokuProject/sudokuIO.h
+++ SudokuProject/sudokuIO.h
@@ -1,14 +1,14 @@
 #ifndef IOER_H
 #define IOER_H
-#include <iostream>
 #include <fstream>
+#include <iostream>
 using namespace std;
 
 class SudokuIOer {
 public:
     SudokuIOer() {
     }
-    void outputFile(int solution[9][9], fstream sudokuFile);
+    void outputFile(int solution[9][9], ofstream& sudokuFile);
 };
 
 #endif
