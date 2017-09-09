ca4a0a0cdfb37cee1c3f97a2dc7f2e26040e8448
Wasdns <wasdnsxchen@gmail.com>
2017-09-09 12:05:26 +0800

    update sudokuIO.cpp: fix sudoku IO bottleneck

--- SudokuProject/sudokuIO.cpp
+++ SudokuProject/sudokuIO.cpp
@@ -1,15 +1,9 @@
 #include "sudokuIO.h"
-#include <fstream>
 #include <stdlib.h>
 #include <iostream>
 using namespace std;
 
-void SudokuIOer::outputFile(int solution[9][9], char filename[]) {
-    fstream sudokuFile;
-    // open sudoku.txt 
-    sudokuFile.open(filename, ios::app);
-
-    // write the answer to sudoku.txt
+void SudokuIOer::outputFile(int solution[9][9], fstream sudokuFile) {
     int i, j;
     for (i = 0; i < 9; i++) {
         for (j = 0; j < 9; j++) {
@@ -21,7 +15,4 @@ void SudokuIOer::outputFile(int solution[9][9], char filename[]) {
         }
     }
     sudokuFile << "\n";
-
-    // close sudoku.txt
-    sudokuFile.close();
 }
