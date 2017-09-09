310a4e4081ef4c8e6b325e339de4d75935df124d
Wasdns <wasdnsxchen@gmail.com>
2017-09-09 13:10:04 +0800

    update sudokuIO.cpp: using ofstream& instead of ofstream

--- SudokuProject/sudokuIO.cpp
+++ SudokuProject/sudokuIO.cpp
@@ -1,9 +1,10 @@
 #include "sudokuIO.h"
 #include <stdlib.h>
+#include <fstream>
 #include <iostream>
 using namespace std;
 
-void SudokuIOer::outputFile(int solution[9][9], fstream sudokuFile) {
+void SudokuIOer::outputFile(int solution[9][9], ofstream& sudokuFile) {
     int i, j;
     for (i = 0; i < 9; i++) {
         for (j = 0; j < 9; j++) {
